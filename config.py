b4ddfe9fbdf6975230105a9377089cc132da6f47
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-05-13 14:21:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]  # Add your Telegram user ID(s) here
+api_id = "27775431"
+api_hash = "b70bb1d45a1d05236671d4cc615e40f9"
+bot_token = "7220112302:AAEWa6X3w3s0ejhM1l2DRhTn7JVdB_IaoPM"
+auth_users = [6414266397]  # Add your Telegram user ID(s) here
